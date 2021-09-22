8ff432ed05e386af44da3cf2b29dcb898117a559
nighmares <estiben.urrea@gmail.com>
2021-09-22 11:02:44 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,15 +1,6 @@
-local server = false
-local menuOpen = false
-
 RegisterNUICallback("dataPost", function(data, cb)
     SetNuiFocus(false)
-    if server then
-        TriggerServerEvent(data.event, data.args)
-        server = false
-    else
-        TriggerEvent(data.event, data.args)
-    end
-    menuOpen = false
+    TriggerEvent(data.event, data.args, data.arg2, data.arg3)
     cb('ok')
 end)
 
@@ -18,17 +9,11 @@ RegisterNUICallback("cancel", function()
 end)
 
 
-RegisterNetEvent('nh-context:sendMenu', function(data, toServer)
+RegisterNetEvent('nh-context:sendMenu', function(data)
     if not data then return end
-    if toServer then server = true end
-    if not menuOpen then
-        SetNuiFocus(true, true)
-        menuOpen = true
-        SendNUIMessage({
-            action = "OPEN_MENU",
-            data = data
-        })
-    else
-       print('A menu of this type is already open.')
-    end
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = "OPEN_MENU",
+        data = data
+    })
 end)
